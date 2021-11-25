b0b0ac187cc88cf55895c23ab37f5d8dd2593cbf
Andrea Guffi <andreaguffi92@gmail.com>
2021-11-25 22:27:36 +0100

    post req

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react'
+import { useFetch } from '../../hooks/useFetch'
 
 import './Create.css'
 
@@ -10,9 +11,11 @@ export default function Create() {
     const [ingredients, setIngredients] = useState([])
     const ingredientInput = useRef(null)
 
+    const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(title, method, cookingTime, ingredients);
+        postData({ title, ingredients, method, cookingTime: cookingTime + ' minutes' })
     }
 
     const handleAdd = (e) => {
