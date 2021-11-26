380f05e48492a0fad549c9c07424e70c64ae9b7f
Andrea Guffi <andreaguffi92@gmail.com>
2021-11-26 22:04:28 +0100

    ok

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,6 +2,11 @@ import './RecipeList.css'
 import { Link } from 'react-router-dom'
 
 export default function RecipeList({ recipes }) {
+
+    if (recipes.length === 0) {
+        return <div className="error">No recipes to load...</div>
+    }
+
     return (
         <div className="recipe-list">
             {recipes.map(recipe => (
